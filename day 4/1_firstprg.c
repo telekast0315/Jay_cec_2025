f2cbbcf524645417013066ea8df8f6c7046da96e
RanjeetaRG <ranjeetargolabhavi@gmail.com>
2025-11-20 09:41:46 +0530

    day4 code pushed

---
+++ day 4/1_firstprg.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()   
+{
+    printf("hllo world");
+    return 0;
+}
---
+++ day 4/3_variables.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+    int age = 30;
+    float marks = 90.00;
+    char gender='M';
+    double pi = 3.14;
+    printf("your age is :%d\n",age);
+    printf("your marks is :%f\n",marks);
+    printf(" yor gender : %c\n",gender);
+    printf("the value of pi :%lf\n", pi);
+    bool iswalking =false;
+    printf("is   walking:%d",iswalking);
+    return 0;
+}
